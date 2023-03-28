774b2534922bc422398a4f668c5f9bd95c2f3993
connorb023 <connorcb@yahoo.com>
2023-03-28 12:36:11 -0600

    set up server and tested port

---
+++ server.js
@@ -0,0 +1,18 @@
+// Import the express module
+const express = require('express');
+
+// Create an instance of the express application
+const app = express();
+
+// Set the port number
+const port = 3000;
+
+// Listen on the specified port and log a message when the server is running
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// Define a GET route for the homepage
+app.get('/', (req, res) => {
+  res.send('Welcome to the Gitpub App!');
+});
